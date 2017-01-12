d1d6e24bc131a91b030fed4d8a4dc9798ca1d956
ABDULLA <shaikhabdul9990@gmail.com>
2017-01-12 15:22:33 +0530

    Addition of session management

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var session = require('express-session')
 var app = express()
 var path = require("path");
 var bodyParser = require('body-parser');
@@ -6,15 +7,28 @@ var LoginService = require('./core/services/loginService');
 app.use(bodyParser.urlencoded({ extended: true })); //body parser 
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({resave:true, saveUninitialized:true, secret: 'ssshhhhh'}));//initialing a session 
+var sess;
 app.get('/',function(req,res){
-	//swig_Template.pagesRendering(req,res);
+	sess=req.session;
+	//console.log(sess.username);
+	if (sess.username){
+		res.redirect('/blogs');
+	}
+	else {
+	//	console.log("if sees.username conditions fails")
 	LoginService.firstPageRendering(req,res);
-});
-//app.set('view engine', 'ejs');
-app.post('/login',function(req,res){
-	
-	LoginService.login(req,res);
 
+	}
+});
+
+
+app.post('/login',function(req,res){
+	sess=req.session;
+	sess.username=req.body.username;
+	//console.log("sess value in login post");
+	console.log(sess.username);
+	LoginService.login(req,res);
 })
 
 app.get('/register',function(req,res)
@@ -28,13 +42,22 @@ app.post('/NewRegister', function(req,res)
 	LoginService.NewRegister(req,res);
 })
 
-
-
 app.get('/blogs', function(req,res)
 {
 	res.sendFile(path.join(__dirname + '/public/blogs.html'));
 
 })
+
+app.get('/logout',function(req,res)
+{
+	req.session.destroy(function(err){
+		if(err) {
+            console.log(err);
+        } else {
+            res.send('successfully logged out');
+	    }
+	});
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
